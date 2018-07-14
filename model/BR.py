3e37f607effcee57e5138b37604634f7dcde5818
Sarthak Consul <32162912+SConsul@users.noreply.github.com>
2018-07-14 12:17:33 +0530

    Update BR.py

--- model/BR.py
+++ model/BR.py
@@ -15,6 +15,7 @@ class BR(nn.Module):
         self.conv2 = nn.Conv2d(out_c,out_c, kernel_size=3,padding=1)
     
     def forward(self,x):
+        x_res = x
 #         x_res = self.bn(x)
 #         x_res = self.relu(x_res)
         x_res = self.conv1(x_res)
